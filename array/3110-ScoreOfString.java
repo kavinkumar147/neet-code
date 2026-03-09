625a966d6c4ee090c7f68d61fc027d6e236cfe44
KAVINKUMAR G <kaving147@gmail.com>
2026-03-09 23:17:25 +0530

    Update 3110-ScoreOfString.java

--- array/3110-ScoreOfString.java
+++ array/3110-ScoreOfString.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int scoreOfString(String s) {
+        int arr[]=new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            arr[i]=(int) s.charAt(i);
+        }
+        int sum=0;
+        for(int i=0;i<s.length() -1;i++){
+        int diff=Math.abs(arr[i]-arr[i+1]);
+        sum=sum+diff;
+        
+        }
+        return sum;
+    }
+}
